e345af2ffbabcd0d6ebcfe25685d37073378b6e2
Mohamed100100 <muhammad.ahmed302@eng-st.cu.edu.eg>
2026-01-23 00:25:22 +0200

    add enums for the policies and this directory will contain any enum in the project

---
+++ include/enums/SinksType.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+
+
+enum class SinkType{
+    CONSOLE,
+    FILE
+};
---
+++ include/enums/TelemetrySource.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+
+// Types of telemetry data sources
+enum class TelemetrySrc_enum {
+    CPU,        // CPU metrics
+    GPU,        // GPU metrics
+    RAM         // Memory metrics
+};
